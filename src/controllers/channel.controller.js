15733a8db7db2e7d715976a950870247f9e2576f
Rosario Garufi <saro.garufi1@gmail.com>
2026-01-16 17:27:53 +0100

    update channel profile

--- src/controllers/channel.controller.js
+++ src/controllers/channel.controller.js
@@ -6,6 +6,10 @@ const ApiResponse = require('../utils/ApiResponse');
 //@route: GET /api/v1/channels/:username
 
 const asyncHandler = require('../utils/asyncHandler');
+const {
+  deleteFromCloudinary,
+  uploadToCloudinary,
+} = require('../utils/cloudinary');
 
 //@Access: Public
 const getChannelInfo = asyncHandler(async (req, res) => {
@@ -33,6 +37,66 @@ const getChannelInfo = asyncHandler(async (req, res) => {
 //@Access: Private
 const updateChannelInfo = asyncHandler(async (req, res) => {
   const { channelDescription, channelTags, socialLinks } = req.body;
+
+  //prepare update object
+  const updateDate = {};
+
+  if (channelDescription !== undefined) {
+    updateDate.channelDescription = channelDescription;
+  }
+
+  if (channelTags !== undefined) {
+    (updateDate.channelDescription = channelDescription),
+      (updateDate.channelTags = Array.isArray(channelTags)
+        ? channelTags
+        : JSON.parse(channelTags));
+  }
+
+  if (socialLinks !== undefined) {
+    updateDate.socialLinks =
+      typeof socialLinks === 'object' ? socialLinks : JSON.parse(socialLinks);
+  }
+
+  //upload coverImage
+  let coverImageUpdate = {};
+
+  if (req?.files?.coverImage?.length > 0) {
+    const coverImageLocalPath = req?.files?.coverImage[0].path;
+    //remove old cover image
+    if (req?.user?.coverImage) {
+      await deleteFromCloudinary(req?.user?.coverImage?.public_id, '/image');
+    }
+
+    //add new cover
+    const uploadResult = await uploadToCloudinary(
+      coverImageLocalPath,
+      'youtube/cover-images'
+    );
+    if (!uploadResult) {
+      throw new ApiError('500', 'Error upload cover image');
+    }
+
+    coverImageUpdate.coverImage = {
+      public_id: uploadResult.public_id,
+      url: uploadResult.secure_url,
+    };
+  }
+  //merge update
+  const updateObject = {
+    ...updateDate,
+    ...coverImageUpdate,
+  };
+
+  //update User
+  const updateUser = await User.findByIdAndUpdate(
+    req?.user?._id,
+    updateObject,
+    { new: true }
+  );
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, updateUser, ' channel update successfull'));
 });
 
 //!DESC: Update channel notification preferences
